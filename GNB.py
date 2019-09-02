d8c7a15a0ada2e663b598ce8ad15f519d99b1ff8
Sgt.Lizard <lizard1998myx@gmail.com>
2019-09-02 09:25:43 +0000

    Update and rename main.py to GNB.py

--- GNB.py
+++ GNB.py
@@ -6,6 +6,7 @@ class Notification(dict):
                  category='', description='', abstract='',
                  note='', status=''):
         dict.__init__(self)
+        self['id'] = 0
         self['source'] = source
         self['time'] = time
         self['deadline'] = deadline
@@ -56,7 +57,7 @@ class Notification(dict):
         return save_message
 
 
-class NotificationCreater():
+class NotificationCreator():
     def __init__(self):
         self.notification = Notification()
 
@@ -77,6 +78,11 @@ class NotificationCreater():
             value = match.groups()[-1]
             self.notification[key] = value
         return self.notification
+        
+    def by_msg(self, msg):
+        self.notification['category'] = re.compile(r'[\[【](.*)[\]】]').search(msg)[1]
+        self.notification['description'] = re.compile(r'[\]】](.*)').search(msg)[1].strip()
+        self.time = datetime.datetime.now().strftime('%m-%d %a %H:%M')
 
 
 class NotificationQueue(list):
@@ -96,7 +102,7 @@ class NotificationQueue(list):
         self.info()
 
     def enqueue(self, notification):
-        self.insert(0, notification)
+        self.append(notification)
 
     def broadcast(self, keylist, model):
         for notification in self:
@@ -105,46 +111,78 @@ class NotificationQueue(list):
     def broadcast_default(self):
         self.broadcast(self._keylist, self._model)
 
-    def show(self):
+    def show(self):  # display the index and notifications
         if len(self) == 0:
             print("队列为空")
+            return "队列为空"
+        msg = ""
         for i in range(len(self)):
-            print(self[i].getmessage(keylist=['category', 'source', 'time'],
-                                     model=[str(i).ljust(3)+'-[' , ']', '|']))
+            msg += self[i].getmessage(keylist=['category', 'source', 'time'],
+                                      model=[str(i).ljust(3)+'-[' , ']', '|'])
+            msg += '\n'
+        print(msg)
+        return msg
 
-    def editor(self):
+    def editor(self):  # edit an existing notification
         self.show()
-        while True:
-            index = input('输入你想修改的通知的数字编号/不修改： ')
-            if index == "":
-                break
-            elif index.isdigit():
-                i = int(index)
-                if i >= 0 and i < len(self):
-                    self[i].editor()
-                else:
-                    print("超出修改范围")
+        index = input('输入你想修改的通知的数字编号/不修改： ')
+        if index == "":
+            return
+        elif index.isdigit():
+            i = int(index)
+            if i in range(0, len(self)):
+                self[i].editor()
             else:
-                print("输入值不合法")
+                print("超出修改范围")
+        else:
+            print("输入值不合法")
+    
+    def remover(self):  # remove a notification
+        self.show()
+        index = input('输入你想delete的通知的数字编号/不修改： ')
+        if index == "":
+            return
+        elif index.isdigit():
+            i = int(index)
+            if i in range(0, len(self)):
+                self.pop(i)
+            else:
+                print("超出修改范围")
+        else:
+            print("输入值不合法")
+            
+    def recent_public(self):  # select notifications published in 7 days
+        new_notq = NotificationQueue()
+        for notification in self:
+            if notification['time'] != "":
+                new_notq.enqueue(notification)
+        return now_notq
 
-
-    def save(self):
+    def save(self, dir=""):  # save files in save_dir, named by index
+        if dir == "":
+            dir = self._save_dir
         for i in range(len(self)):
-            filepath = os.path.join(self._save_dir, str(i)+'.txt')
+            filepath = os.path.join(dir, str(i)+'.txt')
             with open(filepath,'w') as savefile:
                 savefile.write(self[i].save())
 
-    def load(self):
-        for textfile in os.listdir(self._load_dir):
-            filepath = os.path.join(self._load_dir, textfile)
+    def load(self, dir=""):  # read files in load_dir
+        if dir == "":
+            dir = self._load_dir
+        for textfile in os.listdir(dir):
+            filepath = os.path.join(dir, textfile)
             with open(filepath) as loadfile:
-                loadfile.read()
+                self.enqueue(NotificationCreator().by_text(loadfile.read()))
 
     def info(self):
-        version = "GNB_V1.0.3_20190901"
+        version = "GNB_V1.1.1_20190902"
         description = """Full name: Group Notification Broadcasting
 修复录入后不会存档的问题
 未来需要增加删减功能
+put the data in one dir
+make another dir for history
+change log name
+use command to switch mode
 还需要放入帮助信息
 by myx"""
         filepath = os.path.join(self._info_dir, version + '.txt')
@@ -158,9 +196,22 @@ if __name__ == "__main__":
     notq = NotificationQueue()
     notq.load()
     notq.broadcast_default()
-    input("读取完毕！开始修改")
-    notq.editor()
-    input("修改完毕！开始录入")
-    notq.enqueue(NotificationCreater().by_one())
+    input("读取完毕！")
+    while True:
+        command = input('Enter command [Edit, Add, Remove, Loadall, Show, Quit]:')
+        if len(command)==0 or command[0].lower()=='q':
+            break
+        elif command[0].lower()=='e':
+            notq.editor()
+        elif command[0].lower()=='r':
+            notq.remover()
+        elif command[0].lower()=='l':
+            notq.load()
+            notq.load('GNB_save')
+        elif command[0].lower()=='s':
+            notq.show()
+        elif command[0].lower()=='a':
+            notq.enqueue(NotificationCreator().by_one())
     notq.save()
+
     input("进程结束！")
