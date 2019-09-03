c63ac2ae71a01663da36d93b03f3e56260e05911
Sgt.Lizard <lizard1998myx@gmail.com>
2019-09-03 15:37:35 +0800

    Update weather.py

--- awesome/plugins/weather.py
+++ awesome/plugins/weather.py
@@ -1,4 +1,7 @@
 from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, IntentCommand
+from jieba import posseg
+import requests
 
 
 # on_command 装饰器将函数声明为一个命令处理器
@@ -7,7 +10,7 @@ from nonebot import on_command, CommandSession
 async def weather(session: CommandSession):
     # 从会话状态（session.state）中获取城市名称（city），如果当前不存在，则询问用户
     city = session.get('city', prompt='你想查询哪个城市的天气呢？')
-    # 获取城市的天气预报
+    # 获取城市的天气预报 weather_report = await get_weather_of_city(city)
     weather_report = await get_weather_of_city(city)
     # 向用户发送天气预报
     await session.send(weather_report)
@@ -40,4 +43,36 @@ async def _(session: CommandSession):
 async def get_weather_of_city(city: str) -> str:
     # 这里简单返回一个字符串
     # 实际应用中，这里应该调用返回真实数据的天气 API，并拼接成天气预报内容
-    return f'{city}的天气是……'
+    # 已知问题，默认会使用IP所在地的天气
+    r = requests.get('https://www.tianqiapi.com/api/?version=v1&city=' + city + '&appid=1001&appsecret=5578')
+    today_data = r.json()['data'][0]
+    msg = today_data['wea'] + '\n'
+    msg += today_data['tem'] + ' (' + today_data['tem2'] + '-' + today_data['tem1'] + ')\n'
+    msg += 'AQI：' + today_data['air_level'] + ' (' + str(today_data['air']) + ')\n'
+    for index in today_data['index']:
+        if index['title'] in ['紫外线指数', '穿衣指数', '空气污染扩散指数']:
+            msg += index['title'] + ' (' + str(index['level']) + ') ' + index['desc'] + '\n'
+    msg += r.json()['update_time']
+    return city + '的天气 ' + msg
+
+
+# on_natural_language 装饰器将函数声明为一个自然语言处理器
+# keywords 表示需要响应的关键词，类型为任意可迭代对象，元素类型为 str
+# 如果不传入 keywords，则响应所有没有被当作命令处理的消息
+@on_natural_language(keywords={'天气'})
+async def _(session: NLPSession):
+    # 去掉消息首尾的空白符
+    stripped_msg = session.msg_text.strip()
+    # 对消息进行分词和词性标注
+    words = posseg.lcut(stripped_msg)
+
+    city = None
+    # 遍历 posseg.lcut 返回的列表
+    for word in words:
+        # 每个元素是一个 pair 对象，包含 word 和 flag 两个属性，分别表示词和词性
+        if word.flag == 'ns':
+            # ns 词性表示地名
+            city = word.word
+
+    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
+    return IntentCommand(90.0, 'weather', current_arg=city or '')
