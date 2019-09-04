5a53cbec2c90c566be19106feb03cba8f2b1ce3c
Sgt.Lizard <lizard1998myx@gmail.com>
2019-09-04 13:02:03 +0800

    Update grpmsg.py

--- awesome/plugins/grpmsg.py
+++ awesome/plugins/grpmsg.py
@@ -1,4 +1,6 @@
 from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, IntentCommand
+from weather_source_bs4 import Beijing_weather
 
 
 @on_command('send_msg', aliases=('群发',))
@@ -13,6 +15,18 @@ async def send_msg(session: CommandSession):
         for group in group_list:
             # 对某个qq群进行发送信息
             if group['group_id']==230697355:
-                await session.bot.send_group_msg(group_id=group['group_id'], message='群发测试')
+                await session.bot.send_group_msg(group_id=group['group_id'], message=Beijing_weather())
             elif group['group_id']==625122280:
+                continue
                 await session.bot.send_group_msg(group_id=group['group_id'], message='hello world')
+
+
+@on_natural_language(keywords={'发', '说', '讲', '唱', '叫'})
+async def _(session: NLPSession):
+    # 去掉消息首尾的空白符
+    stripped_msg = session.msg_text.strip()
+    while stripped_msg[0] in ['发', '唱', '说', '讲', '个', '首']:
+        stripped_msg = stripped_msg[1:]
+
+    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
+    return IntentCommand(70.0, 'echo', current_arg=stripped_msg)
